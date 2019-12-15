74b95f841b691620336641b4342434e56e19856e
DOM <48492083+AltDom@users.noreply.github.com>
2019-12-15 14:47:51 +1100

    integrate basic folder structure

---
+++ app/users/logout.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+require __DIR__.'/../autoload.php';
+// In this file we logout users.
+
+// Remove the user session variable and redirect the user back to the homepage.
+unset($_SESSION['user']);
+redirect('/');
