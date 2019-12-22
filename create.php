8b60cb587a93bc30fc17478f3a4900e24b44a1df
DOM <48492083+AltDom@users.noreply.github.com>
2019-12-22 11:00:52 +1100

    sych

--- create.php
+++ create.php
@@ -37,7 +37,7 @@
         <div class="form-group">
             <label for="password">Password</label>
             <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <small class="form-text text-muted">Please provide the your password.</small>
         </div><!-- /form-group -->
 
         <button type="submit" class="btn btn-primary">Create and Login</button>
